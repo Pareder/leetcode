21f196ffb5317a8f5f95eb1fcd31985a35a33435
Pareder <ruskc2beat@gmail.com>
2020-05-27 18:42:59 +0300

    Add Maximum Depth of Binary Tree

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ LeetCode problems & solutions
 |67|[Add Binary](https://leetcode.com/problems/add-binary/)|[JavaScript](algorithms/Add%20Binary/index.js)|Easy|
 |70|[Climbing Stairs](https://leetcode.com/problems/climbing-stairs/)|[JavaScript](algorithms/Climbing%20Stairs/index.js)|Easy|
 |100|[Same Tree](https://leetcode.com/problems/same-tree/)|[JavaScript](algorithms/Same%20Tree/index.js)|Easy|
+|104|[Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree/)|[JavaScript](algorithms/Maximum%20Depth%20of%20Binary%20Tree/index.js)|Easy|
---
+++ algorithms/Maximum Depth of Binary Tree/index.js
@@ -0,0 +1,26 @@
+/**
+ * Given a binary tree, find its maximum depth.
+ *
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Input: root = [3, 9, 20, null, null, 15, 7]
+ * Output: 3
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function(root) {
+  return !root ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
